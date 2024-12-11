5dd66cd051576a2093f366807f49f70e820a7feb
oliviadp <olivia.osypovych@gmail.com>
2024-12-11 20:28:12 +0200

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Description from './Description/Description';
+import Options from './Options/Options';
+import Feedback from './Feedback/Feedback';
+import Notification from './Notification/Notification'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+function App () {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiveFeedback = Math.round((feedback.good / totalFeedback) * 100);
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
+
+  const handleReset = () => {
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Description />
+      <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} handleReset={handleReset} />
+      {totalFeedback > 0 ? <Feedback feedback={feedback} totalFeedback={totalFeedback} positiveFeedback={positiveFeedback}/> : <Notification/>}
+    </div>
+  );
+};
+
 
 export default App
---
+++ src/Options/Options.jsx
@@ -0,0 +1,14 @@
+const Options = ({ updateFeedback, totalFeedback, handleReset}) => {
+    
+    return (
+      <div>
+        <button onClick={() => updateFeedback("good")}>Good</button>
+        <button onClick={() => updateFeedback("neutral")}>Neutral</button>
+        <button onClick={() => updateFeedback("bad")}>Bad</button>
+        {totalFeedback > 0 && (<button onClick={handleReset}>Reset</button>)}
+      </div>
+    );
+  };
+
+
+export default Options;
