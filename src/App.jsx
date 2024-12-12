527e9cca066d46a0e32d0f388aec484feb01052f
oliviadp <olivia.osypovych@gmail.com>
2024-12-12 21:03:44 +0200

    finally

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import Description from './Description/Description';
-import Options from './Options/Options';
-import Feedback from './Feedback/Feedback';
-import Notification from './Notification/Notification'
+import Description from './components/Description/Description';
+import Options from './components/Options/Options';
+import Feedback from './components/Feedback/Feedback';
+import Notification from './components/Notification/Notification'
 
 
 
 function App () {
   const [feedback, setFeedback] = useState(() =>
   {
-    const savedFeedback = window.localStorage.getItem("saved-feedback");
+    const savedFeedback = localStorage.getItem("saved-feedback");
 
     if (savedFeedback !== null) {
       return JSON.parse(savedFeedback)
@@ -26,7 +26,7 @@ function App () {
   const positiveFeedback = Math.round((feedback.good / totalFeedback) * 100);
 
   useEffect(() => {
-    window.localStorage.setItem("saved-feedback", JSON.stringify(feedback));
+    localStorage.setItem("saved-feedback", JSON.stringify(feedback));
   }, [feedback]);
 
   const updateFeedback = (feedbackType) => {
